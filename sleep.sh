52df3617f8d5ee5f99559e3ee64fb5ce373e75e1
OSDVF <o.s.dv.f@seznam.cz>
2024-07-14 20:02:02 +0200

    Saving list of previously active cores

--- sleep.sh
+++ sleep.sh
@@ -6,8 +6,9 @@ children() { for f in /proc/$1/task/*/children;do l=""; [ -r $f ] &&read l<$f;fo
 pid_to_name() {
     ps -p $1 -o comm=
 }
-ignore=`cat /home/mousa/Kludges/sleep/ignore.txt`
-do_list="$(cat /home/mousa/Kludges/sleep/do.txt)"
+dir="$(dirname "$BASH_SOURCE")"
+ignore=`cat $dir/ignore.txt`
+do_list="$(cat $dir/do.txt)"
 to_stop=""
 for parent in $do_list; do
 	parent_pid=`pidof $parent`
@@ -39,25 +40,32 @@ done
 
 
 
-echo "$stopped" > /home/mousa/Kludges/sleep/stopped.txt
+echo "$stopped" > $dir/stopped.txt
 prev=""
 prev_p=""
 for c in /sys/devices/system/cpu/cpufreq/policy*; do
 	c_prev=`cat $c/scaling_governor`
-	prev="$prev $c_prev"
-	echo powersave > $c/scaling_governor
-	
-	p_prev=`cat $c/energy_performance_preference`
-	prev_p="$prev_p $p_prev"
-	echo power > $c/energy_performance_preference
+    if [[ $? -eq 0 ]]; then
+        prev="$prev $c_prev"
+        echo powersave > $c/scaling_governor
+        
+        p_prev=`cat $c/energy_performance_preference`
+        prev_p="$prev_p $p_prev"
+        echo power > $c/energy_performance_preference
+    fi
 done
 
-echo "$prev" > /home/mousa/Kludges/sleep/prev_c.txt
-echo "$prev_p" > /home/mousa/Kludges/sleep/prev_p.txt
+echo "$prev" > $dir/prev_c.txt
+echo "$prev_p" > $dir/prev_p.txt
 
+online=""
 # Disable all CPU cores except for 0
 for c in /sys/devices/system/cpu/cpu*/online; do
+    if [ `cat $c` == "1" ]; then
+        online="$online $c"
+    fi
     if [ "$c" != "/sys/devices/system/cpu/cpu0/online" ]; then
         echo 0 > "$c"
     fi
 done
+echo "$online" > $dir/online.txt
--- unsleep.sh
+++ unsleep.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+dir="$(dirname "$BASH_SOURCE")"
 # Enable all CPU cores
-for c in /sys/devices/system/cpu/cpu*/online; do
+online="$(cat $dir/online.txt)"
+for c in $online; do
 	echo 1 > "$c"
 done
 
 echo "About to unpause processes"
-stopped=`cat /home/mousa/Kludges/sleep/stopped.txt`
+stopped=`cat $dir/stopped.txt`
 
 for p in $stopped; do
 	echo "Unpausing $p" 
     kill -CONT $p
 done
 
-prev_p=`cat /home/mousa/Kludges/sleep/prev_p.txt`
-prev_c=`cat /home/mousa/Kludges/sleep/prev_c.txt`
+prev_p=`cat $dir/prev_p.txt`
+prev_c=`cat $dir/prev_c.txt`
 c=0
 for p in $prev_c; do
 	echo $p > /sys/devices/system/cpu/cpufreq/policy$c/scaling_governor
